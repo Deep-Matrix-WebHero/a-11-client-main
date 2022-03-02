bd9e0a4edd10110081f711354c6e90da9e286ab8
Md Ataur Rahman <81749481+Deep-Matrix-WebHero@users.noreply.github.com>
2022-03-03 04:02:16 +0600

    using slice

--- src/components/Packages/Packages.js
+++ src/components/Packages/Packages.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import { useEffect } from 'react';
-import { useState } from 'react';
-import Package from '../Package/Package';
-import './Packages.css';
+import React from "react";
+import {useEffect} from "react";
+import {useState} from "react";
+import Package from "../Package/Package";
+import "./Packages.css";
 
 const Packages = () => {
-    const [items, setItems] = useState([]);
-    useEffect(() => {
-        fetch('https://boiling-eyrie-00422.herokuapp.com/packages')
-            .then(res => res.json())
-            .then(data => setItems(data))
-    }, [])
-    return (
-        <div className="packages-container">
-            {
-                items.map(item => <Package
-                    key={item._id}
-                    item={item}
-                ></Package>)
-            }
-        </div>
-    );
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    fetch("https://boiling-eyrie-00422.herokuapp.com/packages")
+      .then((res) => res.json())
+      .then((data) => setItems(data));
+  }, []);
+  return (
+    <div className="packages-container">
+      {items.slice(0, 6).map((item) => (
+        <Package key={item._id} item={item}></Package>
+      ))}
+    </div>
+  );
 };
 
 export default Packages;
