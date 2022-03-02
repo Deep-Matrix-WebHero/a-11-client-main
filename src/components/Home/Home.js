55c9a71ee372b776cf33197e4d7f731cdaba002a
Md Ataur Rahman <81749481+Deep-Matrix-WebHero@users.noreply.github.com>
2022-03-03 03:59:33 +0600

    home update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import Banner from '../Banner/Banner';
-import Packages from '../Packages/Packages';
-import Resort from '../Resort/Resort';
-import Sports from '../Sports/Sports';
-
+import React from "react";
+import About from "../About/About";
+import Banner from "../Banner/Banner";
+import Faq from "../Faq/Faq";
+import Gallery from "../Gallery/Gallery";
+import Packages from "../Packages/Packages";
+import Resort from "../Resort/Resort";
+import Sports from "../Sports/Sports";
+import Tourplan from "../Tourplan/Tourplan";
 
 const Home = () => {
-    return (
-        <div>
-            <Banner></Banner>
-            <Packages></Packages>
-            <Sports></Sports>
-            <Resort></Resort>
-        </div>
-    );
+  return (
+    <div>
+      <Banner></Banner>
+      <About></About>
+      <Packages></Packages>
+      <Tourplan></Tourplan>
+      <Sports></Sports>
+      <Resort></Resort>
+      <Faq></Faq>
+      <Gallery></Gallery>
+    </div>
+  );
 };
 
 export default Home;
